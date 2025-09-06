65d66fdeceb21304a3627ab07487357afe534a33
Ronkly <vsevolod.maliukov@gmail.com>
2025-09-06 16:40:28 +0200

    Added a little text about the header (TBD text)

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,18 +3,18 @@ import './Header.css';
 function Header () {
     let text = "Get Started";
     let link = "/signup";
-    // This header can be used anywhere so the button text will change based on if the user is logged in or not
+    // This header can be used anywhere but the button text will change based on if the user is logged in or not (whether the user is on the dashboard or not)
     const token = localStorage.getItem('jwt');
     if (token) {
         text = "Log Out";
         link = "/";
     }
+    // Need to add the drop down menu for the dashboard view. Needs several returns based on if the user is logged in or not
     return (
         <header>
             <div className="lside-header">
                 <h1 className='header-title'><span className='red-text'>Split</span>Smart</h1>
             </div>
-
             <div className="rside-header">
                 <a href="/info" className='link header-info-link'>Information</a>
                 <a href={link}>
