5f2115999fb6eed837501990b206f7e40e042b7a
yxzhin <yxzhin@gmail.com>
2025-09-06 11:32:06 +0200

    fix: use slash instead of backslash in conf.py for linux systems

--- server/app/conf.py
+++ server/app/conf.py
@@ -3,6 +3,6 @@ class Config:
     JWT_EXPIRATION_HOURS = 24
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DBNAME = r"db\hzs70finale.sqlite"
+    DBNAME = r"db/hzs70finale.sqlite"
     HOST = "localhost"
     PORT = 5000
