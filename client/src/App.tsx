0426caf5389c9e365ed09ab4f95f9af9af769e70
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-09-06 13:08:52 +0200

    Started hero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css'
 
+// Components
 import Header from './components/Header';
+
+// Pages
+import Hero from './pages/Hero';
 
 function App() {
     return (
         <div className="app">
             <Header />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Hero />} />
+                </Routes>
+            </BrowserRouter>
+
         </div>
     );
 }
---
+++ client/src/pages/Hero.tsx
@@ -0,0 +1,16 @@
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-sec">
+                <div className="lside-hero">
+                    <h1 className="hero-title">
+                        Split <span className="red-text">Bills</span>,<br />
+                        Not <span className="red-text">Friendships</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
