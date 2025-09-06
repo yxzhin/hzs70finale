911751e67e7885a51fce89c517b4f5a08e2428eb
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-09-06 13:37:30 +0200

    Finished Hero Section

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -1,3 +1,5 @@
+import './Hero.css';
+
 function Hero() {
     return (
         <div className="hero">
@@ -7,6 +9,22 @@ function Hero() {
                         Split <span className="red-text">Bills</span>,<br />
                         Not <span className="red-text">Friendships</span>
                     </h1>
+
+                    <p className='hero-subtitle'>
+                        Smart, stress-free expense sharing for roommates and friends
+                    </p>
+
+                    <div className="hero-btns">
+                        <a href="/signup">
+                            <button className="primary-btn hero-cta-btn">Get Started</button>
+                        </a>
+
+                        <a href="/info" className="link hero-info-link">or see how it works</a>
+                    </div>
+                </div>
+
+                <div className="rside-hero">
+                    <img src='/hero-img.png' alt='Hero' className='hero-img' />
                 </div>
             </div>
         </div>
