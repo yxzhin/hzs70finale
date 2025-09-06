06a2961ee2f0149eadf5c2755cbe247186537113
GrygoryZach <grishazschepilo@gmail.com>
2025-09-06 16:31:59 +0200

    feat: Debts and Payments models

--- server/db/models/__all_models.py
+++ server/db/models/__all_models.py
@@ -1,6 +1,9 @@
-from server.db.models import users, groups, user_groups
+from server.db.models import users, groups, user_groups, expenses, payments, debts
 
 User = users.User
 Group = groups.Group
 UserGroup = user_groups.user_groups
 
+Expense = expenses.Expense
+Payment = payments.Payment
+Debt = debts.Debt
