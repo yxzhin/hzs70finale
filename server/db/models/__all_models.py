903f537b58ed765243c81f7f6236474496fd81d9
GrygoryZach <grishazschepilo@gmail.com>
2025-09-06 16:43:03 +0200

    feat: ExpenseParticipant model

--- server/db/models/__all_models.py
+++ server/db/models/__all_models.py
@@ -1,9 +1,10 @@
-from server.db.models import users, groups, user_groups, expenses, payments, debts
+from server.db.models import users, groups, user_groups,expenses, expense_participant, payments, debts
 
 User = users.User
 Group = groups.Group
 UserGroup = user_groups.user_groups
 
 Expense = expenses.Expense
+ExpenseParticipant = expense_participant.ExpenseParticipant
 Payment = payments.Payment
 Debt = debts.Debt
