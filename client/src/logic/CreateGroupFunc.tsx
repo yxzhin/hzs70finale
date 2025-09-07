348ee8fceeda1c69405d7b1eb9007fc1039672a0
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-09-07 03:57:21 +0200

    Added getting token before creating group

--- client/src/logic/CreateGroupFunc.tsx
+++ client/src/logic/CreateGroupFunc.tsx
@@ -10,6 +10,41 @@ function CreateGroupFunc() {
     const navigate = useNavigate();
     const data = location.state as GroupData;
 
+    const token = localStorage.getItem('jwt');
+    useEffect(() => {
+        if (!token) navigate('/', { replace: true });
+    }, [token, navigate]);
+
+    const fetched = useRef(false);
+
+    useEffect(() => {
+        if (fetched.current) return;
+        fetched.current = true;
+        
+        fetch("http://localhost:5000/user/current_user", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then(async (res) => {
+            if (res.status == 404) {
+                navigate('/', { replace: true });
+            } else if (res.status == 500) {
+                localStorage.setItem('jwt', '');
+                navigate('/', { replace: true });
+            } else if (res.status == 200) {
+                const result = await res.json();
+            } else {
+                throw new Error(`Unexpected status: ${res.status}`);
+            }
+        })
+        .catch(err => {
+            console.error("Registration error:", err);
+            navigate("/signup?error=server", { replace: true });
+        });
+    }, [token, navigate]);
+
     const hasFetched = useRef(false);
     useEffect(() => {
         if (hasFetched.current) return;
@@ -23,7 +58,8 @@ function CreateGroupFunc() {
         fetch("http://localhost:5000/groups/", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
             },
             body: JSON.stringify({
                 name: data.name
